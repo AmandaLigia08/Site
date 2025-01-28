e2eb63f809eb3337075fc6193fef86f20cf2ecf7
davidnobreg <davidsantos@ads.fiponline.edu.br>
2025-01-28 09:14:31 -0300

    novo projeto
    subindo arquivos do novo projeto em django para o site ser de luz

---
+++ serdeluz/myapp/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path 
+from myapp import views
+
+urlpatterns = [
+    path('', views.site, name='list-empreendimentos'), 
+
+    #Cadastro
+    #path('form-client/', views.form_client, name='client-create'),  
+
+    #path('form-empreendimento/', views.form_empreendimento, name='empreendimento-create'),  
+    
+    #path('form-empreendimento-arq/<int:id>/', views.form_arquivo, name='empreendimento-arq'),
+    
+    #path('form-venda/<int:id>/', views.form_venda, name='venda-create'),
+    
+
+    #Listagens
+    #path('list_quadra/<int:id>/', views.list_Quadra, name='list-quadras'), 
+
+    #path('list_lote/<int:id>/', views.list_Lote, name='list-lote'), 
+
+    #path('list_cliente/', views.list_Clientes, name='list-clientes'),
+    
+    #path('list_vendas/', views.list_Vendas, name='list-vendras'),
+    
+    
+    #relatorios
+    #path('reports/', views.reports, name='reports'),
+]
