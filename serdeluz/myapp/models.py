f460629ce3b11af8122149ed75f1266c7b1f89dd
davidnobreg <125103172+davidnobreg@users.noreply.github.com>
2025-01-29 10:54:13 -0300

    sobre
    pagina sobre inserida

--- serdeluz/myapp/models.py
+++ serdeluz/myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Conselho(models.Model):
+    name = models.CharField(max_length=100)
+    funcao = models.CharField(max_length=100)
+    instagra = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to='images', verbose_name='Logo',
+        null=True, blank=True)
+    is_ativo = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return "{}".format(self.name,)
+    
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering = ['name']
