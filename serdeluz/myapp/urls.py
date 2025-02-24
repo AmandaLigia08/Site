ec8f6801aaf7a3e499ffc11a85aee668fca72528
Amanda Ligia <amandaaraujo@si.fiponline.edu.br>
2025-02-24 13:37:40 -0300

    valid

--- serdeluz/myapp/urls.py
+++ serdeluz/myapp/urls.py
@@ -4,7 +4,7 @@ from myapp import views
 
 urlpatterns = [
     #paginas
-    path('index/', views.site, name='index'), 
+    path('', views.site, name='index'), 
     path('sobre/', views.sobre, name='sobre'), 
     path('projeto/', views.projeto, name='projeto'),
     path('contato/', views.contato, name='contato'),
